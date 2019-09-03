4d8af20901930e342801afc0547c546bd494d41d
Victor J. Hansen <46825640+vjhansen@users.noreply.github.com>
2019-09-03 17:43:59 +0200

    Update W03__D4.vhd

--- W03__D4.vhd
+++ W03__D4.vhd
@@ -1,30 +1,33 @@
--- need to add clear, enable, load, and 8-bit initial count value (see ffin, ffout)
+-- universal binary counter
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.std_logic_unsigned.all;
 
+entity UniBitCnt is
+  Port (clk, clear, enable, load, direction, reset : in std_logic;
+        c_in : in std_logic_vector(7 downto 0);
+        c_out : out std_logic_vector(7 downto 0));
+end UniBitCnt;
 
-entity w03d4 is
-  Port (clk, dir, rst : in std_logic;
-        cnt_out : out std_logic_vector(7 downto 0));
-end w03d4;
-
-architecture Behavioral of w03d4 is
-    signal cnt : std_logic_vector(7 downto 0);
+architecture Behavioral of UniBitCnt is
+    signal ffin, ffout : std_logic_vector(7 downto 0);
 begin
 
-process (clk, rst)
+process (clk, reset)
 begin
-    if (rst = '1') then
-        cnt <= (others => '0');
+    if (reset = '1') then       -- reset input
+        ffout <= (others => '0');
     elsif rising_edge(clk) then
-        if (dir = '1') then
-            cnt <= cnt + '1'; -- count up
-        else
-            cnt <= cnt - '1'; -- count down
-        end if;
+        ffout <= ffin;
     end if;
 end process;
-cnt_out <= cnt;
+
+ffin <= (others => '0') when (clear = '1')     else -- clear = clear c_out
+        ffout           when (enable = '0')    else    
+        c_in            when (load = '1')      else -- load = load initial value c_in
+        ffout+1         when (direction = '1') else -- count up
+        ffout-1;
+
+c_out <= ffout;
 end Behavioral;

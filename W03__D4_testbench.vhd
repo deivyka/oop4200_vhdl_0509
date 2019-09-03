896932c9e19ed9705ced70ffcb224236f699394f
Victor J. Hansen <46825640+vjhansen@users.noreply.github.com>
2019-09-03 17:44:32 +0200

    Update W03__D4_testbench.vhd

--- W03__D4_testbench.vhd
+++ W03__D4_testbench.vhd
@@ -3,25 +3,27 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
 
-entity w03d4_tb is
-end w03d4_tb;
+entity UniBitCnt_tb is
+end UniBitCnt_tb;
 
-architecture Behavioral of w03d4_tb is
+architecture Behavioral of UniBitCnt_tb is
     constant clk_period : time := 10ns;
 
-component w03d4
-    port (clk, dir, rst: in std_logic;
-          cnt_out: out std_logic_vector(7 downto 0));
+component UniBitCnt
+    port (clk, clear, enable, load, direction, reset : in std_logic;
+        c_in : in std_logic_vector(7 downto 0);
+        c_out : out std_logic_vector(7 downto 0));
 end component;
 
-signal clk, dir, rst : std_logic;
-signal cnt_out : std_logic_vector(7 downto 0);
+signal clk, clear, enable, load, direction, reset : std_logic;
+signal c_in, c_out : std_logic_vector(7 downto 0);
 
 begin
-
-uut: w03d4 port map(
-            clk => clk, dir => dir, rst => rst, 
-            cnt_out => cnt_out
+uut: UniBitCnt port map
+        (
+            clk => clk, clear => clear, enable => enable,
+            load => load, direction => direction, 
+            reset => reset, c_in => c_in, c_out => c_out
         );
 
 clk_process: process
@@ -34,12 +36,18 @@ end process;
 
 stim: process
 begin
-    rst <='1';
+    reset <='1';
+    clear <= '1';
     wait for clk_period;
-    rst <='0';
-    dir <= '1';
+    clear <= '0';
+    reset <='0';
+    enable <= '1';
+    direction <= '1'; -- count up
     wait for clk_period*16;
-    dir <= '0';
+    direction <= '0'; -- count down
     wait for clk_period*16;
+    -- clear <= '1';
+    -- c_in = "0000 1111";
+    wait for clk_period*8;
 end process;
 end;

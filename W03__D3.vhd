c302f02cb074245b11efd192a14b05bd58badac5
Victor J. Hansen <46825640+vjhansen@users.noreply.github.com>
2019-09-03 23:45:04 +0200

    Update W03__D3.vhd

--- W03__D3.vhd
+++ W03__D3.vhd
@@ -1,5 +1,6 @@
 library ieee;
 use ieee.std_logic_1164.all;
+
 entity slr8bits is
     Port ( clk, rst, sin: in std_logic;
            ctrl: in std_logic_vector(1 downto 0);
@@ -9,9 +10,8 @@ entity slr8bits is
 end slr8bits;
 
 architecture arch of slr8bits is
-signal ffin, ffout: std_logic_vector (7 downto 0);
+    signal ffin, ffout: std_logic_vector (7 downto 0);
 begin
-
 -- state register section
 process (clk, rst)
         begin
@@ -21,11 +21,10 @@ process (clk, rst)
                 ffout <= ffin;
             end if;
 end process;
-
 -- outputs section
 with ctrl select
     ffin <=
-        ffout                      when "00",
+        ffout                      when "00",   -- no action
         ffout (6 downto 0) & sin   when "01",   -- left-shift
         sin & ffout(7 downto 1)    when "10",   -- right-shift
         p_load                     when others; -- parallel

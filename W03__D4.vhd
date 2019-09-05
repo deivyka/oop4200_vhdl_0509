596cb8830646fa00f8bce0398873aeba77e3e003
Victor J. Hansen <46825640+vjhansen@users.noreply.github.com>
2019-09-05 11:44:00 +0200

    Update W03__D4.vhd

--- W03__D4.vhd
+++ W03__D4.vhd
@@ -14,6 +14,7 @@ end UniBitCnt;
 architecture Behavioral of UniBitCnt is
     signal ffin, ffout : unsigned(7 downto 0);
 begin
+-- state register section
 process (clk, reset)
 begin
     if (reset = '1') then
@@ -22,10 +23,12 @@ begin
         ffout <= ffin;
     end if;
 end process;
+-- Next-state logic (combinational)
 ffin <= (others => '0') when (clear = '1') else -- clear c_out
         ffout when (enable = '0') else          -- no action
         unsigned(c_in) when (load = '1') else   -- load initial value c_in
         ffout+1 when (direction = '1') else     -- count up
         ffout-1;                                -- count down
+-- output logic (combinational)
 c_out <= std_logic_vector(ffout);
 end Behavioral;
